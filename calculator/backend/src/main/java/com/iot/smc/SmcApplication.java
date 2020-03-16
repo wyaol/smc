e27a6b6084402bb8d2de78a6be1b2e7264ca984f
wyao <289672494@qq.com>
2020-03-17 00:17:35 +0800

    基线代码

---
+++ calculator/backend/src/main/java/com/iot/smc/SmcApplication.java
@@ -0,0 +1,13 @@
+package com.iot.smc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmcApplication.class, args);
+    }
+
+}
